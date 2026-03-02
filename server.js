4b5ba62c35788db006209da904d3b7a5b0e3519f
User <user@example.com>
2026-03-02 23:46:26 +0530

    feat(user-api): task 3 - support GET/user:id with selective field response

--- server.js
+++ server.js
@@ -16,13 +16,43 @@ const users = [
     name: "Bumrah",
     status: "Bowler",
   },
+  {
+    id: 30,
+    name: "Shivam",
+    status: "All-rounder",
+  },
+  {
+    id: 40,
+    name: "Tilak",
+    status: "Batsman",
+  },
+  {
+    id: 50,
+    name: "Surya",
+    status: "Captain",
+  },
 ];
+
 app.get("/user/:id", (req, res) => {
   const userId = Number(req.params.id);
+  const queryFields = req.query.fields;
 
   const user = users.find((u) => u.id === userId);
 
-  res.json(user);
+  if (!user) {
+    return res.status(400).json({ message: "Invalid Id" });
+  }
+
+  const splitFeilds = queryFields.split(",");
+
+  const filtered = {};
+
+  splitFeilds.forEach((f) => {
+    if (user[f] !== undefined) {
+      return (filtered[f] = user[f]);
+    }
+  });
+  res.json(filtered);
 });
 
 app.listen(3000, () => {
