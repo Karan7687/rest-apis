dec01afee43e9dcde660e777cc878f734722fcaa
User <user@example.com>
2026-03-03 19:08:33 +0530

    feat(user-api):task 5: add POST /users endpoint for user creation

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello APIS");
 });
@@ -63,10 +63,25 @@ function checkExist(req, res, next) {
   next();
 }
 
-app.get("/user/:id", validateId, checkExist, (req, res) => {
+app.get("/users/:id", validateId, checkExist, (req, res) => {
   res.json(req.user);
 });
 
+app.post("/users", (req, res) => {
+  const { name, status } = req.body;
+  console.log(req.body);
+  if (!name || !status) {
+    return res.status(400).json({ message: "name or status required" });
+  }
+
+  const newUser = {
+    id: users.length > 0 ? users[users.length - 1].id + 1 : 1,
+    name,
+    status,
+  };
+  users.push(newUser);
+  res.status(200).json(newUser);
+});
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
