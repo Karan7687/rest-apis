39059ec737c1f2ca3e39a45e7c6c2b82ff631cba
User <user@example.com>
2026-03-03 17:12:02 +0530

    feat(user-api):task-4 implement middleware chain for GET/user/:id with ID validation and USER existence checks

--- server.js
+++ server.js
@@ -33,28 +33,49 @@ const users = [
   },
 ];
 
-app.get("/user/:id", (req, res) => {
+app.use((req, res, next) => {
+  const time = new Date().toLocaleTimeString();
+  console.log(`[${req.method}] ${req.url} at ${time}`);
+  next();
+});
+
+function validateId(req, res, next) {
   const userId = Number(req.params.id);
-  const queryFields = req.query.fields;
 
-  const user = users.find((u) => u.id === userId);
-
-  if (!user) {
-    return res.status(400).json({ message: "Invalid Id" });
+  if (isNaN(userId)) {
+    return res.status(400).json({ message: "Id must be a Number" });
   }
 
-  const splitFeilds = queryFields.split(",");
+  req.userId = userId;
+  next();
+}
 
-  const filtered = {};
-
-  splitFeilds.forEach((f) => {
-    if (user[f] !== undefined) {
-      return (filtered[f] = user[f]);
-    }
+function checkExist(req, res, next) {
+  const user = users.find((u) => {
+    return u.id === req.userId;
   });
-  res.json(filtered);
+
+  if (!user) {
+    return res.status(400).json({ message: "User Not Foound" });
+  }
+
+  req.user = user;
+  next();
+}
+
+app.get("/user/:id", validateId, checkExist, (req, res) => {
+  res.json(req.user);
 });
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
+
+/**{
+  params   → from URL
+  query    → from ?key=value
+  body     → from POST data
+  headers  → from request metadata
+  method   → HTTP method
+  url      → full URL
+} */
